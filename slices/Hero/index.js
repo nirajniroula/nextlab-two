5fbe95bc65d3ecf79b7a08bea9f43ca640a21e52
Niraj Niroula <niraj.n@arbyte.com.np>
2023-02-01 13:47:39 +0545

    minor style changes

--- slices/Hero/index.js
+++ slices/Hero/index.js
@@ -24,16 +24,16 @@ const Hero = ({ slice }) => {
       />
       <div className="relative grid justify-items-center gap-6 text-center">
         {prismicH.isFilled.richText(slice.primary.tagline) && (
-          <p className="max-w-lg text-sm font-semibold uppercase tracking-widest text-slate-500">
+          <p className="max-w-lg text-sm font-semibold uppercase tracking-widest text-slate-500 text-black bg-white">
             <PrismicText field={slice.primary.tagline} />
           </p>
         )}
         {prismicH.isFilled.richText(slice.primary.title) && (
-          <h1 className="max-w-3xl text-6xl font-semibold text-white">
+          <h1 className="max-w-3xl text-6xl font-semibold text-black">
             <PrismicText field={slice.primary.title} />
           </h1>
         )}
-        <div className="grid max-w-prose justify-center gap-6">
+        <div className="grid max-w-prose justify-center gap-6 text-white bg-gray-800">
           <PrismicRichText field={slice.primary.text} />
         </div>
         {slice.items.length > 0 && (
